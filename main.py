4d360068e486335d1ab7720aae194906b9eba123
Zhe Zhang <zhezhang@netflix.com>
2023-12-07 12:00:27 -0800

    Update main

--- main.py
+++ main.py
@@ -50,11 +50,11 @@ def inject_ga():
     logging.info(f's {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
     if not soup.find(id=GA_ID):
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)
-        else:
-            shutil.copy(index_path, bck_index)
+        # bck_index = index_path.with_suffix('.bck')
+        # if bck_index.exists():
+        #     shutil.copy(bck_index, index_path)
+        # else:
+        #     shutil.copy(index_path, bck_index)
         html = str(soup)
         new_html = html.replace('<head>', '<head>\n' + GA_JS)
         index_path.write_text(new_html)
